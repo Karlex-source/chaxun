ed9d0223b910227e9d1870d10474d1b2991e53e9
Karlex <99050752+Karlex-source@users.noreply.github.com>
2025-07-14 22:35:19 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         target: 'https://yunyj.linyi.net',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/exam/, '/wechat/imgs'),
-        secure: false,
+        secure: true,
         headers: {
           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -23,7 +23,7 @@ export default defineConfig({
         target: 'https://yunyj.linyi.net',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/exam-page/, '/wechat/imgs'),
-        secure: false,
+        secure: true,
         headers: {
           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
@@ -33,7 +33,7 @@ export default defineConfig({
         target: 'https://yunyj.linyi.net',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/proxy/, ''),
-        secure: false,
+        secure: true,
         headers: {
           'Accept': '*/*',
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
